305793ff33528816d017119f21c548ab8e0296d8
dr-data <data@9lab.co>
2017-07-24 00:32:52 +0800

    Section 3 Lecture 21

--- server/main.js
+++ server/main.js
@@ -8,4 +8,16 @@ Meteor.startup (function () {
   //let square = (x) => x*x;
   //console.log("square = ", square(10));
 
+  // let user = {
+  //   name: 'test',
+  //   sayHi () {
+  //     setTimeout(() => {
+  //       console.log(this.name)
+  //     }, 1000);
+  //   }
+  // };
+
+  let numbers = [9, 99, 4, 56];
+    let newNumbers = numbers.map((number) => number +=1);
+    console.log(newNumbers);
 });
