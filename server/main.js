d3e5abf4fe01efe4d7802addc84d97f313160215
dr-data <data@9lab.co>
2017-07-25 16:48:34 +0800

    Section 4 Lecture 26, solved a challenge

--- server/main.js
+++ server/main.js
@@ -2,4 +2,23 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup (() => {
+  class Person{
+    constructor(name = 'Anomymous', age = 0) {
+      this.name = name;
+      this.age = age;
+    }
+    getGreeting(){
+      // Hi! I am Andrew.
+      //return 'Hi! Iam' + this.name +'.';
+      return `Hi! I am ${this.name}.`;
+    }
+    getAge(){
+      return `Hi! I am ${this.name} is ${this.age} year (s) old.`
+    }
+  }
+  // let me = new Person('mike');
+  // console.log(me.getAge());
+
+  let me = new Person('Andrew', 25);
+  console.log(me.getAge());
 });
