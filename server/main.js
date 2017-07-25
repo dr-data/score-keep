6d6851092a915d39552033928b096ebde501cb31
dr-data <data@hku.hk>
2017-07-25 22:38:21 +0800

    Section 3 Lecture 28

--- server/main.js
+++ server/main.js
@@ -1,24 +1,65 @@
 import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
-Meteor.startup (() => {
-  class Person{
-    constructor(name = 'Anomymous', age = 0) {
+Meteor.startup(() => {
+  class Person {
+    constructor(name = 'Anonymous', age = 0) {
       this.name = name;
       this.age = age;
     }
-    getGreeting(){
-      // Hi! I am Andrew.
-      //return 'Hi! Iam' + this.name +'.';
+    getGreeting() {
       return `Hi! I am ${this.name}.`;
     }
-    getAge(){
-      return `Hi! I am ${this.name} is ${this.age} year (s) old.`
+    getPersonDescription() {
+      return `${this.name} is ${this.age} year(s) old.`;
     }
   }
-  // let me = new Person('mike');
-  // console.log(me.getAge());
 
-  let me = new Person('Andrew', 25);
-  console.log(me.getAge());
+  class Employee extends Person {
+  	constructor(name, age, title) {
+  		super(name, age);
+  		this.title = title;
+  	}
+  	getGreeting (){
+  		if (this.title) {
+  			return 'path1 ' + `Hi! Iam ${this.name}. I work as a ${this.title}`
+
+  		} else {
+  			return 'path2 ' + super.getPersonDescription();
+
+  		}
+  	}
+  	hasJob (){
+  		return !!this.title;
+  	} 
+  }
+
+   class Programmer extends Person {
+   		constructor(name, age, language = 'assembly') {
+   			super(name, age);
+   			this.language = language;
+   		}
+   		getGreeting (){
+   			if (this.language) {
+   				return `Hi! Iam ${this.name}. I am a ${this.language} developer.`;
+   			} else {
+   				return super.getGreeting();
+   			}
+   		}
+  }
+
+
+  let me = new Employee('Mike', 26, 'db admin', 'assembly');
+  console.log(me.getGreeting());
+  console.log(me.getGreeting());
+  console.log('has Job:' + me.hasJob());
+
+  let person = new Employee('Andrew');
+  console.log(person.getPersonDescription());
+  console.log(person.hasJob());
+  console.log(person.getGreeting());
+
+  let userOne = new Programmer(q	);
+   console.log(userOne.getGreeting());
+
 });
